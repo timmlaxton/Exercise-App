c8bd0e391c3dc04344695097cffe6198fbc05a7d
Tim <timmlaxton@gmail.com>
2019-11-19 17:51:52 +0000

    connected backend to frontend

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
   constructor(props) {
@@ -27,6 +28,9 @@ export default class CreateUsers extends Component {
       username: this.state.username,
 
     }
+
+    axios.post('http://localhost:5000/users/add', user)
+    .then(res => console.log(res.data));
 
     console.log(user)
 
